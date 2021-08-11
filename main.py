33ec3488585d03d09cc1a0dafa3e6d70c44c920d
gonzoden <deniz.nazarova.99@gmail.com>
2021-08-11 14:10:07 +0600

    add initial code

---
+++ main.py
@@ -0,0 +1,25 @@
+import checkBox
+
+#creating a diary-file
+try:
+    quit_f = "not"
+    f = open("diary.txt", "r")
+    print(f.read())
+
+    while quit_f != "poka":
+        f = open("diary.txt", "a")
+        quit_f = input()
+        f.write(quit_f)
+        f.write("\n")
+
+    if (checkBox.isAlone("diary.txt")): #TODO add function
+        print("Do you need help?")
+
+except:
+    print("It seems that you do not possess a required diary-file. Please reload the program in order to read/write data")
+    f = open("diary.txt", "w")
+    f.write("Please, type 'poka' in order to exit the file  ")
+    f.write("\n")
+    f. close()
+
+#TODO: how to work with spreadshit
